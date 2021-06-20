76a21ec19db00f26c183cb26d50591df84531f8f
nirmalkoffice <77260962+nirmalkoffice@users.noreply.github.com>
2021-06-20 17:00:56 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from "@angular/common/http";
+import { NavComponent } from './nav/nav.component';
+import { SearchComponent } from './search/search.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SearchresultComponent } from './searchresult/searchresult.component';
+import { CardComponent } from './card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SearchComponent,
+    PortfolioComponent,
+    SearchresultComponent,
+    CardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
